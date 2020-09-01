5caf08c8b93518137ace1d3854779f9b9c1fedcf
mukulsitap <mukulsitap@github.com>
2020-09-01 09:48:51 +0530

    second commit

--- passwordchecker.py
+++ passwordchecker.py
@@ -3,7 +3,7 @@ import hashlib
 import sys
 
 def request_api_data(query_char):
-	url = 'https://api.pwnedpasswords.com/range/' + query_char
+	url = 'https://api.pwnedpasswords.com/range/' + query_char  
 	res = requests.get(url)
 	if res.status_code != 200:
 		raise RuntimeError(f'Error fetching: {res.status_code}', ch)
@@ -17,8 +17,8 @@ def get_password_leaks_count(hashes, hash_to_check):
 	return 0
 
 def pwned_api_check(password):
-	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()	
-	first5_char, tail = sha1password[:5], sha1password[5:]
+	sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()  #create a hash string	
+	first5_char, tail = sha1password[:5], sha1password[5:] 
 	response = request_api_data(first5_char)
 	return get_password_leaks_count(response, tail)
 
@@ -29,5 +29,6 @@ def main(args):
 			print(f'{password} was found {count} times.. change it!!')
 		else:
 			print(f'{password} was not found. Carry on!!!')
-
-main(sys.argv[1:])
+	return "done!"
+if __name__ == '__main__' :
+	sys.exit(main(sys.argv[1:]))
